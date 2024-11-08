630b170dab7f07ea8c879a00c1a33b13641e2ac8
Mschukin <mshhukin@yandex.ru>
2024-11-08 15:08:12 +0300

    release1.1

--- client/vite-project/src/components/LoadTheme.jsx
+++ client/vite-project/src/components/LoadTheme.jsx
@@ -1,34 +1,28 @@
-import { useEffect, useState } from "react"
-import Theme from "./Theme"
+import { useEffect, useState } from "react";
+import Theme from "./Theme";
 
+function LoadTheme() {
+  const [theme, setTheme] = useState([]);
 
+  useEffect(() => {
+    fetchThemes();
+  }, []);
 
+  async function fetchThemes() {
+    const response = await fetch("/api/themes");
+    const data = await response.json();
 
-function LoadTheme(){
-const [theme,setTheme] = useState([])
-
-
-
-useEffect(()=>{
-    fetchThemes()
-},[])
-
-async function fetchThemes(){
-
-const response = await fetch('/api/themes')
-const data = await response.json()
-
-setTheme(data)
-}
-    return(
-<>
-<h1>Выбор темы:</h1>
-{
-theme.map(el=><Theme key ={el.id} cardProps = {el} setTheme = {setTheme}/>)
+    setTheme(data);
+  }
+  return (
+    <>
+      <h1>Выбор темы:</h1>
+      {theme.map((el) => (
+        <Theme key={el.id} cardProps={el} setTheme={setTheme} />
+      ))}
+    </>
+  );
 }
 
-</>
-    )
-}
+export default LoadTheme;
 
-export default LoadTheme
--- client/vite-project/src/components/Theme.jsx
+++ client/vite-project/src/components/Theme.jsx
@@ -1,22 +1,23 @@
-
-function Theme({cardProps,setTheme}){
-console.log(cardProps);
+function Theme({ cardProps, setTheme }) {
 
 
-return (
-<>
-<div className="card mb-3" style={{ width: '18rem' }}>
-            <div className="card-body">
-                <h5 className="card-title">{cardProps.theme}</h5>
-                <button  className="btn btn-danger">Выбрать</button>
-            </div>
+
+async function fetchQuest() {
+
+const response = await fetch(`/api/themes/${cardProps.id}`)
+
+  }
+
+  return (
+    <>
+      <div className="card mb-3" style={{ width: "18rem" }}>
+        <div className="card-body">
+          <h5 className="card-title">{cardProps.theme}</h5>
+          <button onClick={fetchQuest} className="btn btn-danger">Выбрать</button>
         </div>
-    
-</>
-)
-
-
+      </div>
+    </>
+  );
 }
 
-
-export default Theme
+export default Theme;
